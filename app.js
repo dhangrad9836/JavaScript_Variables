2df2eb7a42f6bc0970317f4c82811416f02afa73
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-04-07 22:43:32 -0400

    Completed JavaScript exercise

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Darren";
+let lastName;
+var age;
 // Exercise 2
+lastName = "Dhanpat";
+age = 44;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnsale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 465;
+let hasRead = true;
